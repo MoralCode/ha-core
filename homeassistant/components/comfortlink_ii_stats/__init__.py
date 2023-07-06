83ed8b71af6b8c4d563f6a7cc844fa574d422b40
Adrian Edwards <adrian@adriancedwards.com>
2023-07-05 18:03:31 -0700

    merge async_setup_entry values with ChatGPT suggestions

--- homeassistant/components/comfortlink_ii_stats/__init__.py
+++ homeassistant/components/comfortlink_ii_stats/__init__.py
@@ -29,7 +29,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     hass.data[DOMAIN][entry.entry_id] = {"trane_client": trane}
 
 
-    coordinator = ComfortLinkCoordinator(hass, my_api)
+    coordinator = ComfortLinkCoordinator(hass, entry)
 
     # Fetch initial data so we have data when entities subscribe
     #
@@ -41,6 +41,9 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     #
     await coordinator.async_config_entry_first_refresh()
 
+    # Start the socket connection and data processing in a separate thread or asyncio task
+    threading.Thread(target=start_socket_connection, args=(hass, coordinator)).start()
+    
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
 
     async_add_entities(
@@ -48,6 +51,23 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     )
 
     return True
+
+def start_socket_connection(hass, coordinator, trane):
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    loop.run_until_complete(socket_listener(hass, coordinator, trane))
+    loop.close()
+
+async def socket_listener(hass, coordinator, trane):
+    # Create a socket connection to your device and listen for data
+    # Use your Python library to process the received raw bytes into a class
+    # Update the coordinator with the new compressor speed value whenever it changes
+    # Example:
+    # for data in trane.listen():
+    while True:
+        raw_data = await my_device.receive_data()
+        compressor_speed = my_device.process_data(raw_data)
+        coordinator.async_set_compressor_speed(compressor_speed)
 
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
