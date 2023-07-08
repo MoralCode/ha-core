30116e9ddefc3a6a771f072de32c869f220a1bfd
Adrian Edwards <adrian@adriancedwards.com>
2023-07-08 08:59:08 -0700

    fix the writing of data into HA

--- homeassistant/components/comfortlink_ii_stats/sensor.py
+++ homeassistant/components/comfortlink_ii_stats/sensor.py
@@ -68,7 +68,7 @@ class ComfortLink2Sensor(SensorEntity):
 
     def _update_data(self, data):
         """ Tell HomeAssistant that new data is available"""
-        self.hass.states.set(DOMAIN + ".compressor_speed", data.cmp_spd)
+        self._attr_native_value = data.cmp_spd
         # self.state.native_value
         self.async_schedule_update_ha_state()
     
